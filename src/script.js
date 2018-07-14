89bf7fee1f6385ce43fd1883e2dbbd01f1cfe3db
David <davidzhoucanada@gmail.com>
2018-07-14 13:50:14 -0400

    Add functionality for adding items

--- src/script.js
+++ src/script.js
@@ -1,43 +1,32 @@
-const addAssignments = document.querySelector('#add');
-const assignmentsList = document.querySelector('#assignments');
-var assignments = JSON.parse(localStorage.getItem('assignments')) || [];
+document.addEventListener('DOMContentLoaded', function () {
+    const addAssignments = document.querySelector('#add');
+    const assignmentsList = document.querySelector('#assignments');
+    var assignments = JSON.parse(localStorage.getItem('assignments')) || [];
 
-addAssignments.addEventListener('submit', addAssignment);
-assignmentsList.addEventListener('click', toggleDone);
+    addAssignments.addEventListener('click', addAssignment);
 
-function addAssignment(e) {
-    e.preventDefault();
-    const assignment = {
-    text: (this.querySelector('[name=assignment]')).value,
-    done: false
+    function addAssignment(e) {
+        e.preventDefault();
+        const assignment = {};
+        assignments.push(assignment);
+        populateList(assignments, assignmentsList);
+        localStorage.setItem('assignments', JSON.stringify(assignments));
+        this.reset();
     }
-    assignments.push(assignment);
-    populateList(assignments, assignmentsList);
-    localStorage.setItem('assignments', JSON.stringify(assignments));
-    this.reset();
-}
 
-function populateList(assignments = [], assignmentsList) {
-    assignmentsList.innerHTML = assignments.map((assignment, i) => {
-    return `
-        <li>
-            <input type="text" data-index=${i} id="name">
-            <input type="text" data-index=${i} id="weight">
-            <input type="text" data-index=${i} id="mark">
-            <button id="delete" data-index=${i} class="foo-button mdc-button">✕</button>
-        </li>
-    `;
-    }).join('');
-}
-
-function toggleDone(e) {
-    if (!e.target.matches('input')) {
-    return;
+    function populateList(assignments = [], assignmentsList) {
+        assignmentsList.innerHTML = assignments.map((assignment, i) => {
+            return `
+            <li>
+                <input type="text" data-index=${i} id="name">
+                <input type="text" data-index=${i} id="weight">
+                <input type="text" data-index=${i} id="mark">
+                <button id="delete" data-index=${i} class="foo-button mdc-button">✕</button>
+            </li>
+        `;
+        }).join('');
     }
-    const index = e.target.dataset.index;
-    assignments[index].done = !assignments[index].done;
-    localStorage.setItem('assignments', JSON.stringify(assignments));
-    populateList(assignments, assignmentsList);
-}
 
-populateList(assignments, assignmentsList);
+    populateList(assignments, assignmentsList);
+
+});
