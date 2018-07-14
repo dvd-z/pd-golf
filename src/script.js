ae254d62eb1d61a2b15f6830e36304e3b62fd443
David <davidzhoucanada@gmail.com>
2018-07-14 15:41:13 -0400

    Update populateList to correspond with updated HTML

--- src/script.js
+++ src/script.js
@@ -1,16 +1,18 @@
 const addAssignments = document.querySelector("#add");
 const assignmentsList = document.querySelector("#assignments");
+const calculate = document.querySelector("#calculate");
 var assignments = JSON.parse(localStorage.getItem("assignments")) || [];
 
 addAssignments.addEventListener("click", addAssignment);
 assignmentsList.addEventListener("click", deleteItem);
 assignmentsList.addEventListener("input", modifyItem);
+calculate.addEventListener("click", calculateMarks);
 
 function addAssignment() {
     const assignment = {
-        name: "test",
-        weight: 0,
-        mark: 0
+        name: "Assignment",
+        weight: "",
+        mark: ""
     };
     assignments.push(assignment);
     populateList(assignments, assignmentsList);
@@ -21,8 +23,7 @@ function modifyItem(e) {
     if (!e.target.matches("input") || e.target.matches("input#delete")) {
         return;
     }
-    const index = e.target.dataset.index;
-    assignments[index][e.target.id] = e.target.value;
+    assignments[e.target.dataset.index][e.target.id] = e.target.value;
     localStorage.setItem("assignments", JSON.stringify(assignments));
 }
 
@@ -35,14 +36,29 @@ function deleteItem(e) {
     localStorage.setItem("assignments", JSON.stringify(assignments));
 }
 
+function calculateMarks() {
+
+}
+
 function populateList(assignments = [], assignmentsList) {
     assignmentsList.innerHTML = assignments.map((assignment, i) => {
         return `
             <li>
-                <input type="text" data-index=${i} id="name" value=${assignments[`${i}`].name}>
-                <input type="text" data-index=${i} id="weight" value=${assignments[`${i}`].weight}>
-                <input type="text" data-index=${i} id="mark" value=${assignments[`${i}`].mark}>
-                <input type="submit" id="delete" data-index=${i} class="foo-button mdc-button" value="✕">
+                <form class="form-inline">
+                    <div class="form-group">
+                        <input type="text" data-index=${i} class="form-control" id="name" value=${assignments[`${i}`].name}>
+                        <label for="name" class="bmd-label-floating">Assignment Name</label>
+                    </div>
+                    <div>
+                        <input type="text" data-index=${i} class="form-control" id="weight" value=${assignments[`${i}`].weight}>
+                        <label for="weight" class="bmd-label-floating">Weight</label>
+                    </div>
+                    <div>
+                        <input type="text" data-index=${i} class="form-control" id="mark" value=${assignments[`${i}`].mark}> 
+                        <label for="mark" class="bmd-label-floating">Mark</label>
+                    </div>
+                    <input type="submit" id="delete" data-index=${i} value="✕">
+                </form>
             </li>
         `;
     }).join("");
